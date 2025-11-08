0bf49de4f413b3dc3f8ac651142e26c60cc3b553
Tanush T M <tanushtm.work@gmail.com>
2025-11-08 13:13:29 +0530

    docker file added

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello :)</h1>"
+    return "<h1>hello world :)</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', 
